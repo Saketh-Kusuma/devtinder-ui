6e15d36180dc90eec68a53cb7eabfb9b71545ada
K-SAKETH1 <kusumasaketh92@gmail.com>
2025-10-02 21:55:27 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import "./App.css"
+import Navbar from "./components/Navbar"
 function App() {
   return (
     <>
-      <h1>Helo World</h1>
+    <Navbar/>
+      <h1 className="text-3xl ">Helo World</h1>
+      <button className="btn btn-primary">Button</button>
+
     </>
   )
 }
