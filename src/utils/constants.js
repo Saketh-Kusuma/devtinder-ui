13278825adb586ddb3ce3044336d556a2f4aea0a
K-SAKETH1 <kusumasaketh92@gmail.com>
2025-10-08 10:49:24 +0530

    Minor Changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
-  BASE_URL: "http://localhost:3000",
+  BASE_URL: "/api",
   DEFAULT_PHOTO:"https://img.freepik.com/free-vector/user-blue-gradient_78370-4692.jpg?semt=ais_hybrid&w=740&q=80"
 
 };
